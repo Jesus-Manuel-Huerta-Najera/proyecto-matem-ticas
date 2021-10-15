a238a2606932efe18a5d9d2500dd32b5178abfc6
Jesus Huerta <1718110393@utectulancingo.edu.mx>
2021-10-15 17:33:00 -0500

    Cambios despues del uso del stash

--- matematicas.md
+++ matematicas.md
@@ -1,2 +1,4 @@
+
 Esto es una modificacion al fichero matematicas
+se realizo un cambio al fichero de matematicas.
 uso de stash
--- suma.py
+++
@@ -1,4 +0,0 @@
-a = int(input("Escribe el primer numero: "))
-b = int(input("Escribe el segundo numero: "))
-Res = a + b
-print("El resultado es: " + str(Res))
